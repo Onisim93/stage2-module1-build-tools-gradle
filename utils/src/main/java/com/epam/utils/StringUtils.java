13a18e17c19f8383a2276767898583362067ba1f
onisim_albarov <onisim93@gmail.com>
2022-12-16 13:22:30 +0400

    fix StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,6 +3,16 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
-        return Integer.parseInt(str) > 0;
+        try {
+            double number = Double.parseDouble(str);
+            if (number > 0) {
+                return true;
+            }
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+
+        return false;
     }
 }
