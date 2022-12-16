51d962165fe824905638c2f87e5e1baf1869e3e7
onisim_albarov <onisim93@gmail.com>
2022-12-16 13:14:01 +0400

    fix Utils.java

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,9 +5,17 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         //magic happens here
-        for (String s : args) {
-            int number = Integer.parseInt(s);
-            if (number < 1) return false;
+
+        if (args.size() == 0) return false;
+
+        try {
+            for (String s : args) {
+                double number = Double.parseDouble(s);
+                if (number < 1) return false;
+            }
+        }
+        catch (NumberFormatException e) {
+            return false;
         }
 
         return true;
